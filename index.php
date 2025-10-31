c3d7bbf9e5b683e59455b728419cf9443a70003e
hernan527 <hernan527@gmail.com>
2025-10-31 11:21:20 -0300

    semgef

---
+++ index.php
@@ -0,0 +1,22 @@
+
+<?php 
+
+// require './contador.php';
+
+// $contador = new contador();
+
+// if (!($contador->ipaddress_exist())) {
+// 	 $contador->add_ipaddress(); 
+// }
+
+// $ip_user = $contador->getRealIP();
+// $numeroDeVisitas = $contador->numeroDeVisitas();
+
+require 'index.view.php';
+
+?>
+			 
+        
+
+						
+            
